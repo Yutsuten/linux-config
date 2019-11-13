ed9e69d7c7821a0534c7bf756dca514d4e1231a2
Yutsuten <mateus.etto@gmail.com>
2019-11-13 20:58:36 +0900

    Add alias for bell

--- bash/mylibrary.sh
+++ bash/mylibrary.sh
@@ -3,6 +3,7 @@
 # Some useful alias and functions
 
 alias cpwd='echo -n $(pwd) | xclip -selection c'
+alias bell='mpv --really-quiet /usr/share/sounds/freedesktop/stereo/complete.oga'
 
 branch() {
   local git_status="$(git status 2> /dev/null)"
