585f49835e18c1c7705f40efd702bbe5b7363891
Yutsuten <mateus.etto@gmail.com>
2019-11-18 13:27:23 +0900

    Add alias for notification

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,3 @@
 alias cpwd='echo -n $(pwd) | xclip -selection c'
 alias bell='mpv --really-quiet /usr/share/sounds/freedesktop/stereo/complete.oga'
+alias notify='notify-send -i /usr/share/icons/breeze-dark/apps/64/utilities-terminal.svg "Terminal" "Command finished!" && bell'
