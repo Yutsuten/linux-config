944f27993c65745138270b85ad0f9a1f0eb1e469
Yutsuten <mateus.etto@gmail.com>
2020-03-21 23:29:31 +0900

    Nmake and notify are not critical anymore

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,3 @@
 alias cpwd='echo -n $(pwd) | xclip -selection c'
 alias bell='mpv --really-quiet /usr/share/sounds/freedesktop/stereo/complete.oga'
-alias notify='notify-send -u critical -a Konsole -i /usr/share/icons/breeze-dark/apps/64/utilities-terminal.svg "Terminal" "Your command has finished!" && bell'
+alias notify='notify-send -a Konsole -i /usr/share/icons/breeze-dark/apps/64/utilities-terminal.svg "Terminal" "Your command has finished!" && bell'
