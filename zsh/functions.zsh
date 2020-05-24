f064d6c54384d3e987ae6ed78feb6fa9d5feb2f9
Yutsuten <mateus.etto@gmail.com>
2020-05-24 16:49:43 +0900

    Update volume of countdown

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,6 +1,6 @@
 countdown() {
   termdown -B -f doh \
-    --exec-cmd 'mpv --really-quiet /usr/share/sounds/Smooth/stereo/count-down.oga' \
+    --exec-cmd 'mpv --really-quiet --volume=75 /usr/share/sounds/Smooth/stereo/count-down.oga' \
     $1 && \
   fellow finish
 }
