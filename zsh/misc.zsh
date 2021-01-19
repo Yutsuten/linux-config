26b2b09aa4d141e861f7495079798bed93878fbc
Yutsuten <mateus.etto@gmail.com>
2021-01-19 23:07:20 +0900

    Add custom way to change Anki profile with ZSH

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -4,3 +4,19 @@ function termcolors() {
     print -P "%F{${COLOR1}}Color${COLOR1}  %F{${COLOR2}}Color${COLOR2}"
   done
 }
+
+function ankiprofile() {
+  case $1 in
+    mateus)
+      ln -snf ~/Anki/mateus "${HOME}/.local/share/Anki2"
+      print -P '%F{6}✓ Switched to profile mateus.'
+      ;;
+    dev)
+      ln -snf ~/Anki/dev "${HOME}/.local/share/Anki2"
+      print -P '%F{6}✓ Switched to profile dev.'
+      ;;
+    *)
+      print -P "%F{1}× Invalid Anki profile." >&2
+      ;;
+  esac
+}
