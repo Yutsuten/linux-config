21d8325b68e37134ed59921c18577409a4feff48
Yutsuten <mateus.etto@gmail.com>
2021-02-17 18:56:49 +0900

    Disable ZSH correction

--- zsh/run_commands.zsh
+++ zsh/run_commands.zsh
@@ -6,7 +6,6 @@ ZSH_THEME="yutsuten"
 HYPHEN_INSENSITIVE="true"
 DISABLE_AUTO_UPDATE="true"
 DISABLE_AUTO_TITLE="true"
-ENABLE_CORRECTION="true"
 HIST_STAMPS="yyyy-mm-dd"
 
 plugins=(git virtualenv ssh-agent zsh-syntax-highlighting)
