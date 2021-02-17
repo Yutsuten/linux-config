3734a28aa636df0ed6e9be80607799f616f41d0c
Yutsuten <mateus.etto@gmail.com>
2021-02-17 11:42:16 +0900

    Fix zshrc

--- zsh/run_commands.zsh
+++ zsh/run_commands.zsh
@@ -1,5 +1,5 @@
 export GPG_TTY=$(tty)
-export ZSH="/home/mateus/.oh-my-zsh"
+export ZSH="${HOME}/.oh-my-zsh"
 
 ZSH_THEME="yutsuten"
 
@@ -11,4 +11,4 @@ HIST_STAMPS="yyyy-mm-dd"
 
 plugins=(git virtualenv ssh-agent zsh-syntax-highlighting)
 
-source $ZSH/oh-my-zsh.sh
+source "${ZSH}/oh-my-zsh.sh"
