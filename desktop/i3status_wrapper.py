d62c905111ffe28deebdd2d7f609bb502a0d7742
Yutsuten <mateus.etto@gmail.com>
2021-02-23 20:20:55 +0900

    Refactor i3status_wrapper.py

--- desktop/i3status_wrapper.py
+++ desktop/i3status_wrapper.py
@@ -1,57 +1,49 @@
 #!/usr/bin/env python
+"""A i3status wrapper to output custom commands to it."""
 
 import json
 import subprocess
 import sys
 
 
+def main():
+    """Inserts extra information into i3status."""
+    print_line(read_line())
+    print_line(read_line())
+    while True:
+        line, prefix = read_line(), ''
+        if line.startswith(','):
+            line, prefix = line[1:], ','
+
+        status = json.loads(line)
+
+        current_kb = run_program(['xkblayout-state', 'print', '%s'])
+        status.insert(6, {'full_text': current_kb, 'name': 'curkb'})
+
+        print_line(prefix+json.dumps(status))
+
+
 def run_program(args):
-    """ Run an arbitrary program. """
-    result = subprocess.run(args, text=True, capture_output=True)
+    """Run an arbitrary program."""
+    result = subprocess.run(args, text=True, check=True, capture_output=True)
     return result.stdout.strip()
 
 
 def print_line(message):
-    """ Non-buffered printing to stdout. """
+    """Non-buffered printing to stdout."""
     sys.stdout.write(message + '\n')
     sys.stdout.flush()
 
 
 def read_line():
-    """ Interrupted respecting reader for stdin. """
-    # try reading a line, removing any extra whitespace
+    """Interrupted respecting reader for stdin."""
     try:
         line = sys.stdin.readline().strip()
-        # i3status sends EOF, or an empty line
         if not line:
             sys.exit(3)
         return line
-    # exit on ctrl-c
     except KeyboardInterrupt:
-        sys.exit()
-
-
-def main():
-    """ Inserts extra information into i3status. """
-    # Skip the first line which contains the version header.
-    print_line(read_line())
-
-    # The second line contains the start of the infinite array.
-    print_line(read_line())
-
-    while True:
-        line, prefix = read_line(), ''
-        # ignore comma at start of lines
-        if line.startswith(','):
-            line, prefix = line[1:], ','
-
-        j = json.loads(line)
-
-        current_kb = run_program(['xkblayout-state', 'print', '%s'])
-        j.insert(4, {'full_text': current_kb, 'name': 'curkb'})
-
-        # and echo back new encoded json
-        print_line(prefix+json.dumps(j))
+        sys.exit(0)
 
 
 if __name__ == '__main__':
