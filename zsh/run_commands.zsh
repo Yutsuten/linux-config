61ab307a828f831d207f503c5f8fd9efc47ca027
Yutsuten <mateus.etto@gmail.com>
2021-09-30 12:16:41 +0900

    Enable lscolors and more adjusts to zsh config

--- zsh/run_commands.zsh
+++ zsh/run_commands.zsh
@@ -1,62 +1,41 @@
 export GPG_TTY=$(tty)
 export LC_MESSAGES=C
-export ZSH_CONF="${HOME}/.linux_config/zsh"
-
-HISTFILE="$HOME/.zsh_history"
-HISTSIZE=50000
-SAVEHIST=10000
-
-setopt PROMPT_SUBST
-setopt APPENDHISTORY
-setopt EXTENDED_HISTORY       # record timestamp of command in HISTFILE
-setopt HIST_EXPIRE_DUPS_FIRST # delete duplicates first when HISTFILE size exceeds HISTSIZE
-setopt HIST_IGNORE_DUPS       # ignore duplicated commands history list
-setopt HIST_IGNORE_SPACE      # ignore commands that start with space
+export LSCOLORS="Gxfxcxdxbxegedabagacad"
 
 autoload -Uz compinit
+autoload -U colors
 compinit
+colors
+
+ZSH_CONF="${HOME}/.linux_config/zsh"
+HISTFILE="$HOME/.zsh_history"
+HISTSIZE=10000
+SAVEHIST=10000
+
+setopt prompt_subst           # Enable parameter expansion, command substitution and arithmetic expansion to prompt
+setopt extended_history       # Record timestamp of command in HISTFILE
+setopt hist_expire_dups_first # Delete duplicates first when HISTFILE size exceeds HISTSIZE
+setopt hist_ignore_dups       # Ignore duplicated commands history list
+setopt hist_ignore_space      # Ignore commands that start with space
+setopt sharehistory           # Share history across terminals
 
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z-_}' 'r:|=*' 'l:|=* r:|=*'
 zstyle ':completion:*' menu select
 zstyle ':completion:*' special-dirs true
-zstyle ':completion:*' list-colors '=*=34'
+zstyle ':completion:*' list-colors '=*=39'
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
 
 function precmd() {
+  # Set the terminal title to the current directory name
   echo -ne "\033]0;$(basename $PWD)\007"
 }
 
-function termcolors() {
-  for COLOR1 in {0..7}; do
-    COLOR2=$((COLOR1+8))
-    print -P "%F{${COLOR1}}Color${COLOR1}  %F{${COLOR2}}Color${COLOR2}"
-  done
-}
-
-function aur_update() {
-  WHITE="\033[0;37m"
-  NOCOLOR="\033[0m"
-  (
-    cd "${HOME}/Packages/AUR"
-    count=$(ls -1 | wc -l)
-    cur=1
-    for package in *; do
-      (
-        cd "${package}"
-        echo -e "${WHITE}[${cur}/${count}] Building ${package}${NOCOLOR}"
-        echo -e "${WHITE}> git pull${NOCOLOR}"
-        git pull
-        echo -e "${WHITE}> makepkg${NOCOLOR}"
-        makepkg --nocolor --syncdeps --install --needed --clean || true
-      )
-      (( cur+=1 ))
-    done
-  )
-}
-
 source "${ZSH_CONF}/key-bindings.zsh"
+source "${ZSH_CONF}/custom_commands.zsh"
 source "${ZSH_CONF}/prompt.zsh"
 source "${ZSH_CONF}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 
+alias ls='ls --color=tty'
 alias ll='ls -hl'
 alias ~='cd ~'
 alias ..='cd ..'
