3eb0061a7ca172da5970b03e0f987fd111cd41b7
Yutsuten <mateus.etto@gmail.com>
2022-03-19 22:29:24 +0900

    Remove notification from screenshot

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -12,16 +12,13 @@ IMG_NAME="Screenshot_$(date '+%Y-%m-%dT%H:%M:%S.%N' | cut -b -23).png"
 case $1 in
   --full)
     import -window root "${SCREENSHOT_DIR}/${IMG_NAME}"
-    notify-send -a Screenshot -i "${SCREENSHOT_DIR}/${IMG_NAME}" 'Full screen capture' "File saved to ${SCREENSHOT_DIR}"
     ;;
   --active)
     WIN_ID="$(xprop -root _NET_ACTIVE_WINDOW | cut -d ' ' -f 5)"
     import -window "${WIN_ID}" "${SCREENSHOT_DIR}/${IMG_NAME}"
-    notify-send -a Screenshot -i "${SCREENSHOT_DIR}/${IMG_NAME}" 'Active window capture' "File saved to ${SCREENSHOT_DIR}"
     ;;
   --select)
     import "${SCREENSHOT_DIR}/${IMG_NAME}"
-    notify-send -a Screenshot -i "${SCREENSHOT_DIR}/${IMG_NAME}" 'Selected area capture' "File saved to ${SCREENSHOT_DIR}"
     ;;
   *)
     printf "Invalid option.\n" >&2
