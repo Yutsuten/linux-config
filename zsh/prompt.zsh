7b4546547f16b4b357bc70570b801dc41ccd2e02
Yutsuten <mateus.etto@gmail.com>
2022-03-30 22:38:45 +0900

    Improve zsh performance by simplifying git prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -12,11 +12,7 @@ git_prompt() {
 
   local title
   if [[ $ret == 0 ]]; then
-    if [[ -n "$(git show-ref origin/${branch#refs/heads/} 2> /dev/null)" ]]; then
-      title="☁ ${branch#refs/heads/}"
-    else
-      title="↻ ${branch#refs/heads/}"
-    fi
+    title="🢰 ${branch#refs/heads/}"
   else
     local label=$(git for-each-ref --points-at=HEAD --count=1 --format='%(refname:short)' refs/tags 2> /dev/null)
     if [[ -n ${label} ]]; then
@@ -26,30 +22,10 @@ git_prompt() {
       title="◉ ${commit_hash}"
     fi
   fi
-
-  local color="%F{2}"
-  if [[ -n $(git status --porcelain 2> /dev/null | tail -1) ]]; then
-    color="%F{1}"
-  elif [[ -n "$(git rev-list origin/${branch#refs/heads/}..HEAD 2> /dev/null)" ]] || [[ -n "$(git rev-list HEAD..origin/${branch#refs/heads/} 2> /dev/null)" ]]; then
-    color="%F{3}"
-  fi
-
-  local extra=""
-  if git rev-parse --git-dir &> /dev/null; then
-    local commits_ahead="$(git rev-list --count @{upstream}..HEAD 2> /dev/null)"
-    local commits_behind="$(git rev-list --count HEAD..@{upstream} 2> /dev/null)"
-    if [[ -n "${commits_ahead}" && "${commits_ahead}" != 0 ]]; then
-      extra+=" 🠥${commits_ahead}"
-    fi
-    if [[ -n "${commits_behind}" && "${commits_behind}" != 0 ]]; then
-      extra+=" 🠧${commits_behind}"
-    fi
-  fi
-
-  echo -n " ${color}(${title}${extra})%f"
+  echo -n " %F{7}(${title})%f"
 }
 
-local user_host="%B[%D %*] %F{13}%n@%m%b"
+local user_host="%B[%D %*] %F{6}%n@%m%b"
 local curdir="%F{14}%~%f"
 
 PROMPT="%f
