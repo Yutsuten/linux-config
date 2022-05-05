aa6ddc2c1b77df69030120d09923b17de4ebcf0b
Yutsuten <mateus.etto@gmail.com>
2022-05-05 11:18:34 +0900

    Fix fish config user PATH

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set -g PATH $PATH:$HOME/.local/bin:$HOME/.yarn/bin
+    set -U fish_user_paths ~/.local/bin ~/.yarn/bin
     set -g GPG_TTY $(tty)
 end
