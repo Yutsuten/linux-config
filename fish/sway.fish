ca3e4c6d50849e7dd894a9ce25004d962c7129c1
Yutsuten <mateus.etto@gmail.com>
2022-05-06 14:51:49 +0900

    GTK theme Arc

--- fish/sway.fish
+++ fish/sway.fish
@@ -1,7 +1,7 @@
 set TTY1 (tty)
 if [ "$TTY1" = "/dev/tty1" ]
   # GTK theme
-  gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Dark'
+  gsettings set org.gnome.desktop.interface gtk-theme 'Arc'
   gsettings set org.gnome.desktop.interface icon-theme 'Arc'
   gsettings set org.gnome.desktop.interface font-name 'Noto Sans 12'
   gsettings set org.gnome.desktop.interface cursor-theme 'Vimix-cursors'
