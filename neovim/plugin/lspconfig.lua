4d9c409d3f6f8851eb5875d81b913044f01d5d26
Yutsuten <mateus.etto@gmail.com>
2022-05-21 23:15:42 +0900

    Update nvim diagnostics signs

--- neovim/plugin/lspconfig.lua
+++ neovim/plugin/lspconfig.lua
@@ -18,10 +18,10 @@ vim.api.nvim_set_keymap('n', '<space>k', '<cmd>lua vim.lsp.buf.hover()<CR>', opt
 
 -- diagnostics signs
 local signs = {
-  Error = ' ✗',
-  Warn = ' ‼',
-  Info = ' 𝒾',
-  Hint = ' 𝒾',
+  Error = '✗',
+  Warn = '‼',
+  Info = '𝒾',
+  Hint = '●',
 }
 
 for type, icon in pairs(signs) do
@@ -32,8 +32,8 @@ end
 vim.cmd [[
   highlight DiagnosticSignError ctermfg=1 ctermbg=0
   highlight DiagnosticSignWarn ctermfg=3 ctermbg=0
-  highlight DiagnosticSignInfo ctermfg=13 ctermbg=0
-  highlight DiagnosticSignHint ctermfg=13 ctermbg=0
+  highlight DiagnosticSignInfo ctermfg=4 ctermbg=0
+  highlight DiagnosticSignHint ctermfg=2 ctermbg=0
 ]]
 
 -- diagnostics virtual text
