c364c4de5d584f750e1b6e119dc9eed2c6cd647d
Yutsuten <mateus.etto@gmail.com>
2022-05-28 17:46:05 +0900

    Update fish configuration

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,15 @@
 if status is-interactive
+    fish_add_path ~/.local/bin ~/.yarn/bin
+
     set -gx GPG_TTY $(tty)
     set -gx NNN_OPTS eor
     set -gx NNN_PLUG 'd:dragdrop;i:img_shuffle'
 
-    set -g fish_user_paths ~/.local/bin ~/.yarn/bin
     set -g fish_greeting
     set -g fish_color_user blue
     set -g fish_color_host brmagenta
     set -g fish_color_host_remote yellow
 
+    alias identify 'identify -precision 3'
     alias ssh 'env TERM=xterm-256color ssh'
 end
