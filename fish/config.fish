4cb40838ef2688358f3723fc5f37530ff2efc0cd
Yutsuten <mateus.etto@gmail.com>
2022-05-28 16:19:15 +0900

    Fish prefer global instead of universal vars

--- fish/config.fish
+++ fish/config.fish
@@ -3,8 +3,8 @@ if status is-interactive
     set -gx NNN_OPTS eor
     set -gx NNN_PLUG 'd:dragdrop;i:img_shuffle'
 
-    set -U fish_user_paths ~/.local/bin ~/.yarn/bin
-    set -U fish_greeting
+    set -g fish_user_paths ~/.local/bin ~/.yarn/bin
+    set -g fish_greeting
     set -g fish_color_user blue
     set -g fish_color_host brmagenta
     set -g fish_color_host_remote yellow
