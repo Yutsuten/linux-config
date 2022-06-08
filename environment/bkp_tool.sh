5b7a46fb33ce7933952875d78b29410322bcc08e
Yutsuten <mateus.etto@gmail.com>
2022-06-08 14:18:56 +0900

    Fix tar warning

--- environment/bkp_tool.sh
+++ environment/bkp_tool.sh
@@ -49,7 +49,7 @@ if [[ ${BACKUP} = 1 ]]; then
         rsync --archive --update --delete --exclude 'gamemode*.mp4' "${HOME}/${dir}/" "${LOCAL_BKP_DIR}/${dir}/"
       done
       echo '[Local sync] Backup osu!stable'
-      tar -czf "${LOCAL_BKP_DIR}/osu-stable.tgz" /usr/local/games/osu
+      tar -czf "${LOCAL_BKP_DIR}/osu-stable.tgz" -C /usr/local/games osu
       echo '[Local sync] Finish'
     } &
   fi
