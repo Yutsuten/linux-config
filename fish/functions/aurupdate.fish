6d8ca6dbb3cca7d0c46982dabc82d161335bfef1
Yutsuten <mateus.etto@gmail.com>
2022-11-12 00:23:09 +0900

    Update aurupdate output

--- fish/functions/aurupdate.fish
+++ fish/functions/aurupdate.fish
@@ -1,18 +1,14 @@
 function aurupdate --description 'Update AUR packages'
+    echo ':: Update AUR packages'
     set -l count $(ls -1 ~/.local/aur | wc -l)
     set -l cur 1
     for aur_dir in ~/.local/aur/*
-        set_color white
-        echo "[$cur/$count] Update $(basename "$aur_dir")"
-        echo '> git pull'
-        set_color normal
+        echo "($cur/$count) Check updates for $(basename $aur_dir)"
+        echo 'git pull'
         git -C "$aur_dir" pull
-
-        set_color white
-        echo '> makepkg'
-        set_color normal
+        echo 'makepkg'
         fish -c "cd $aur_dir && makepkg -sic --needed --nocolor"
-
+        echo
         set -l cur $(math $cur + 1)
     end
 end
