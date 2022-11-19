fc36f0285ae75a8380bd238dc9da88ae2f36f663
Yutsuten <mateus.etto@gmail.com>
2022-11-19 23:21:36 +0900

    Move pipewire init script

---
+++ pipewire/pipewire-init.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+
+sleep 0.2
+pactl set-default-sink alsa_output.pci-0000_2f_00.4.analog-stereo
--- window_manager/pipewire-init.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/sh
-
-sleep 0.5
-pactl set-default-sink alsa_output.pci-0000_2f_00.4.analog-stereo
-
-sleep 1
-pactl load-module module-loopback sink=mix source=alsa_output.pci-0000_2f_00.4.analog-stereo.monitor
-pactl load-module module-loopback sink=mix channels=2 channel_map=mono,mono source=alsa_input.pci-0000_2f_00.4.analog-stereo
