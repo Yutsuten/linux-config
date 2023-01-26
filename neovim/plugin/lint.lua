0c7ca20c37438a460653bade9c29d36dba9e0a09
Yutsuten <mateus.etto@gmail.com>
2023-01-26 18:32:17 +0900

    Apply eslint for jsx and tsx

--- neovim/plugin/lint.lua
+++ neovim/plugin/lint.lua
@@ -1,10 +1,12 @@
 require('lint').linters_by_ft = {
   javascript = {'eslint'},
+  javascriptreact = {'eslint'},
   typescript = {'eslint'},
+  typescriptreact = {'eslint'},
   sh = {'shellcheck'},
   vim = {'vint'},
   lua = {'luacheck'},
-  yaml = {'yamllint'}
+  yaml = {'yamllint'},
 }
 
 vim.cmd([[
