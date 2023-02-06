0712e73e1f6eb5168b78a0f9c6429cf71b026f04
Yutsuten <mateus.etto@gmail.com>
2023-02-06 17:17:25 +0900

    Update ll alias
    Including git data is slow sometimes

--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -1,3 +1,3 @@
 function ll --wraps=exa --description 'List contents of directory using long format'
-    command exa -l --group-directories-first --git $argv
+    command exa -l --group-directories-first $argv
 end
