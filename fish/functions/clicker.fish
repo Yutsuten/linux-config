410126d98acb301363ea5cdb49874fe3b49b27f6
Yutsuten <mateus.etto@gmail.com>
2023-02-20 09:21:25 +0900

    Remove sleep between press and release (clicker)

--- fish/functions/clicker.fish
+++ fish/functions/clicker.fish
@@ -2,7 +2,6 @@ function clicker --description 'Autoclicker for Sway'
     sleep $argv[1] || return
     while true
         swaymsg seat seat0 cursor press button1
-        sleep 0.01s
         swaymsg seat seat0 cursor release button1
         sleep $argv[1]
     end
