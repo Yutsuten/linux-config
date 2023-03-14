f756bcce7a7d01a73320991ff73d859f5caf4268
Yutsuten <mateus.etto@gmail.com>
2023-03-14 20:02:18 +0900

    Small update to pipewire-init.sh

--- pipewire/pipewire-init.sh
+++ pipewire/pipewire-init.sh
@@ -3,5 +3,5 @@
 sleep 0.2
 pactl set-default-sink "$(pactl list short sinks | sed -nE 's/^.*(alsa_output.+analog-stereo[^\s\t]*).*$/\1/p')"
 
-sleep 0.1
+sleep 0.2
 wpctl set-volume @DEFAULT_AUDIO_SINK@ 45%
