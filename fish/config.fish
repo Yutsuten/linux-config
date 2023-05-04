37335aeb20d11a0aa138f3d3c64dfd6d6549918b
Yutsuten <mateus.etto@gmail.com>
2023-05-04 17:19:03 +0900

    Configure pyenv

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,6 @@
 if status is-interactive
-    fish_add_path ~/.local/bin
-
     set -gx GPG_TTY (tty)
+    set -gx PYENV_ROOT $HOME/.cache/pyenv
     set -g fish_color_user blue
     set -g fish_color_host brmagenta
     set -g fish_color_host_remote yellow
@@ -9,6 +8,9 @@ if status is-interactive
 
     alias identify 'identify -precision 3'
     alias ssh 'env TERM=xterm-256color ssh'
+
+    fish_add_path ~/.local/bin $PYENV_ROOT/bin
+    pyenv init - | source
 end
 
 function fish_greeting
