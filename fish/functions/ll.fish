806e3dfdd89d21b70622d9bef586c07dabdf8404
Yutsuten <mateus.etto@gmail.com>
2023-06-01 10:11:57 +0900

    Fish: re-enable ll shortcut

--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -1,5 +1,3 @@
 function ll --wraps=ls --description 'List contents of directory using long format'
-    echo "Temporarily disabled. Use 'lo' instead."
-    return 1
     command ls -l --human-readable --literal --classify=auto --group-directories-first --sort=v --color=auto $argv
 end
