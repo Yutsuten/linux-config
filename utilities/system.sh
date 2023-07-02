4e42834ab1962b25aacaec1fe0eda08ac1eb5750
Yutsuten <mateus.etto@gmail.com>
2023-07-02 16:12:21 +0900

    Use Arc icons on system.sh

--- utilities/system.sh
+++ utilities/system.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-option=$(echo ' Lock
- Logout
- Sleep
- Hibernate
- Reboot
- Poweroff' | wofi --dmenu --prompt '' --cache-file /dev/null | cut -d ' ' -f 2)
+option=$(echo 'img:/usr/share/icons/Arc/panel/22/nm-vpn-standalone-lock.svg:text:Lock
+img:/usr/share/icons/Arc/panel/22/avatar-default.svg:text:Logout
+img:/usr/share/icons/Arc/panel/22/user-status-pending.svg:text:Sleep
+img:/usr/share/icons/Arc/panel/22/network-error.svg:text:Hibernate
+img:/usr/share/icons/Arc/panel/22/network-transmit-receive.svg:text:Reboot
+img:/usr/share/icons/Arc/panel/22/system-devices-panel.svg:text:Poweroff' | wofi --dmenu --prompt '' --cache-file /dev/null | cut -d ':' -f 4)
 
 case ${option} in
   'Lock')
