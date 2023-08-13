8433d53486b6e5479dd38401f0b18e9e8da8b2c5
Yutsuten <mateus.etto@gmail.com>
2023-08-13 22:24:54 +0900

    Small echo adjusts to bkptool

--- fish/functions/bkptool.fish
+++ fish/functions/bkptool.fish
@@ -16,29 +16,31 @@ function bkptool --description 'Backup and restore tool'
     end
 
     if set -ql _flag_restore
-        echo 'Restoring from local drive...'
+        echo 'Start restore'
         for dir in $sync_dirs
+            echo "Syncing $dir"
             rsync --archive --update --delete "$bkp_dir/$dir/" "$HOME/$dir/"
         end
     else
+        echo 'Start backup'
         if set -ql _flag_gpg
-            echo '> Export and encrypt GPG secret keys'
+            echo 'Generate GPG secret keys backup'
             gpg --armor --export-secret-keys 281E7046D5349560 | gpg --output "$HOME/Documents/GPG/gpg-master-keys.asc.gpg" --yes --symmetric -
         end
 
-        echo '> Generate 100% Orange Juice save data backup'
+        echo 'Generate 100% Orange Juice save data backup'
         mkdir /tmp/100OJ_Save_Data
         cp -a ~/.steam/steam/steamapps/common/'100 Orange Juice'/user* /tmp/100OJ_Save_Data
         tar --zstd -cf "$HOME/Documents/100OJ_Save_Data.zst" -C /tmp 100OJ_Save_Data
         rm -rf /tmp/100OJ_Save_Data
 
-        echo 'Start backup'
         for dir in $sync_dirs
             echo "Syncing $dir"
             rsync --archive --update --delete "$HOME/$dir/" "$bkp_dir/$dir/"
         end
+
         echo 'Backup osu!lazer'
         tar --zstd -cf "$bkp_dir/osu-lazer.tar.zst" -C ~/.local/share osu
-        echo 'Finish backup'
     end
+    echo 'Finish!'
 end
