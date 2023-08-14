7274308c2121b8487bc0a86379f7b778b5b9df46
Yutsuten <mateus.etto@gmail.com>
2023-08-14 20:43:07 +0900

    nnn copy filename ;c to ;y

--- fish/functions/nnn.fish
+++ fish/functions/nnn.fish
@@ -3,7 +3,7 @@ function nnn --wraps=nnn --description 'The unorthodox terminal file manager.'
         echo 'nnn is already running'
         return
     end
-    set -x NNN_PLUG 'c:-!wl-copy "$nnn"*;d:dragdrop;i:-vimiv;p:-mpv'
+    set -x NNN_PLUG 'y:-!wl-copy "$nnn"*;d:dragdrop;i:-vimiv;p:-mpv'
     set -x NNN_BMS 'm:/media;l:/mnt/hdd'
     set -x NNN_TRASH 1
     command nnn -AeouUT v $argv
