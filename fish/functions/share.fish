d0ae64d11e84eebf6aa7ebc2aa1310612c71fbbc
Yutsuten <mateus.etto@gmail.com>
2023-08-18 21:02:57 +0900

    Fish add share command

---
+++ fish/functions/share.fish
@@ -0,0 +1,14 @@
+function share --description 'Share screen as a camera using v4l2loopback'
+    if test (count $argv) -eq 0 || test $argv[1] != 'camera' && test $argv[1] != 'screen'
+        echo 'Usage: share [camera|screen]' >&2
+        return 1
+    end
+    sudo modprobe -r v4l2loopback
+    sudo modprobe v4l2loopback
+    sleep 0.1
+    if test $argv[1] = 'camera'
+        droidcam-cli -hflip 192.168.0.110 4747
+    else
+        yes | wf-recorder --muxer=v4l2 --codec=rawvideo --file=/dev/video0 -x yuv420p -F hflip -t
+    end
+end
