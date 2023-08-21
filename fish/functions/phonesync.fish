89f3bb4b4b2854f9ccf32ef443850a4e4598274c
Yutsuten <mateus.etto@proton.me>
2023-08-21 22:49:23 +0900

    Phonesync musics

--- fish/functions/phonesync.fish
+++ fish/functions/phonesync.fish
@@ -4,15 +4,20 @@ function phonesync
         return 1
     end
 
-    set options --inplace --omit-dir-times --no-perms --update --recursive --delete
+    set bold (tput bold)
+    set reset (tput sgr0)
+    set options --recursive --inplace --size-only --delete --omit-dir-times --no-perms --verbose
 
-    echo 'Sync notes to phone'
+    echo -s $bold '> Syncing notes' $reset
     rsync $options ~/Documents/Notes/ /media/mtp/SDカード/Documents/
-
-    echo 'Sync photos to phone'
+    echo
+    echo -s $bold '> Syncing musics' $reset
+    rsync $options ~/Music/ /media/mtp/SDカード/Music/
+    echo
+    echo -s $bold '> Syncing photos' $reset
     rsync $options ~/Pictures/Photos/ /media/mtp/SDカード/DCIM/Camera/Photos/
-
-    echo 'Sync videos to phone'
+    echo
+    echo -s $bold '> Syncing videos' $reset
     rsync $options ~/Videos/Camera/ /media/mtp/SDカード/DCIM/Camera/Videos/
 
     echo 'Finish!'
