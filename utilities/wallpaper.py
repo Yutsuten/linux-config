1b0ca681283ab36e20343bb6b847fd8af75fff34
Yutsuten <mateus.etto@proton.me>
2023-09-16 15:15:38 +0900

    Update screenshot and wallpaper path
    Also:
    - Update wallpaper script to fix ruff rules

--- utilities/wallpaper.py
+++ utilities/wallpaper.py
@@ -1,91 +1,94 @@
 #!/usr/bin/env python
 """Set wallpaper in sway."""
 
+from __future__ import annotations
+
 import argparse
 import os
 import random
 import subprocess
 import sys
+from pathlib import Path
+from typing import Literal
 
-DIRECTORY = os.path.expanduser('~/Pictures/Images/Wallpapers/Active')
-HISTORY_PATH = os.path.expanduser('~/.cache/wallpaper')
+WALLPAPERS_PATH = Path.expanduser(Path('~/Pictures/Wallpapers'))
+HISTORY_FILE_PATH = Path.expanduser(Path('~/.cache/wallpaper'))
 
 
-def main(**argv):
+def main(**argv: bool) -> Literal[0, 1]:
     """Main function."""
     if argv['random']:
-        result = set_random_wallpaper()
-    elif argv['restore']:
-        result = restore_wallpaper()
-    return result
+        return set_random_wallpaper()
+    if argv['restore']:
+        return restore_wallpaper()
+    return 1
 
 
-def set_random_wallpaper():
+def set_random_wallpaper() -> Literal[0, 1]:
     """Change to a random wallpaper."""
-    wallpaper_list = os.listdir(DIRECTORY)
-    if not wallpaper_list:
+    wallpapers = os.listdir(WALLPAPERS_PATH)
+    wallpapers = [wallpaper for wallpaper in wallpapers if Path.is_file(WALLPAPERS_PATH / Path(wallpaper))]
+    if not wallpapers:
         return 1
 
-    if os.path.isfile(HISTORY_PATH):
-        with open(HISTORY_PATH, 'r', encoding='utf-8') as history_file:
+    if Path.is_file(HISTORY_FILE_PATH):
+        with Path.open(HISTORY_FILE_PATH, 'r', encoding='utf-8') as history_file:
             history = history_file.read().splitlines()
     else:
         history = []
 
-    while len(history) > len(wallpaper_list) / 2:
+    while len(history) > len(wallpapers) / 2:
         history.pop()
 
-    candidates = set(wallpaper_list) - set(history)
-    elected = random.choice(tuple(candidates))
+    candidates = set(wallpapers) - set(history)
+    elected = random.choice(tuple(candidates))  # noqa: S311
     history.insert(0, elected)
-    if len(history) > len(wallpaper_list) / 2 + 1:
+    if len(history) > len(wallpapers) / 2 + 1:
         history.pop()
 
-    with open(HISTORY_PATH, 'w', encoding='utf-8') as history_file:
+    with Path.open(HISTORY_FILE_PATH, 'w', encoding='utf-8') as history_file:
         history_file.write('\n'.join(history))
 
-    elected_path = os.path.join(DIRECTORY, elected)
+    wallpaper_path = WALLPAPERS_PATH / Path(elected)
     try:
         subprocess.run(
-            ['swaymsg', 'output', '*', 'bg', elected_path, 'fill'],
-            check=True
+            ['/usr/bin/swaymsg', 'output', '*', 'bg', wallpaper_path, 'fill'],  # noqa: S603
+            check=True,
         )
     except subprocess.CalledProcessError:
-        print('Failed to apply wallpaper. Sway is not running.')
+        print('Failed to apply wallpaper. Sway is not running.')  # noqa: T201
     return 0
 
 
-def restore_wallpaper():
+def restore_wallpaper() -> Literal[0, 1]:
     """Restore the last wallpaper used.
 
     Fallback to a random wallpaper if the last wallpaper is not available.
     """
-    if not os.path.isfile(HISTORY_PATH):
+    if not Path.is_file(HISTORY_FILE_PATH):
         return set_random_wallpaper()
 
-    with open(HISTORY_PATH, 'r', encoding='utf-8') as history_file:
+    with Path.open(HISTORY_FILE_PATH, 'r', encoding='utf-8') as history_file:
         wallpaper = history_file.readline().strip()
-    wallpaper_path = os.path.join(DIRECTORY, wallpaper)
+    wallpaper_path = WALLPAPERS_PATH / Path(wallpaper)
 
-    if not os.path.isfile(wallpaper_path):
+    if not Path.is_file(wallpaper_path):
         return set_random_wallpaper()
 
-    subprocess.run(
-        ['swaymsg', 'output', '*', 'bg', wallpaper_path, 'fill'],
-        check=True
-    )
+    try:
+        subprocess.run(
+            ['/usr/bin/swaymsg', 'output', '*', 'bg', wallpaper_path, 'fill'],  # noqa: S603
+            check=True,
+        )
+    except subprocess.CalledProcessError:
+        print('Failed to apply wallpaper. Sway is not running.')  # noqa: T201
     return 0
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Set wallpaper in sway.')
+    parser = argparse.ArgumentParser(description='Set wallpaper in sway.')
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument(
-        '--random', action='store_true',
-        help='Change to a random wallpaper.')
-    group.add_argument(
-        '--restore', action='store_true',
-        help='Restore the last wallpaper used.')
+    group.add_argument('--random', action='store_true', help='Change to a random wallpaper.')
+    group.add_argument('--restore', action='store_true', help='Restore the last wallpaper used.')
     args = parser.parse_args()
     sys.exit(main(**vars(args)))
