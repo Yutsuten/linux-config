0e6c28aa88792ce63be72b4e29b5bbaadb0252cc
Yutsuten <mateus.etto@proton.me>
2023-09-17 18:38:14 +0900

    Update phonesync

--- fish/functions/phonesync.fish
+++ fish/functions/phonesync.fish
@@ -2,40 +2,44 @@ function phonesync
     set bold (tput bold)
     set reset (tput sgr0)
 
+    set documents ~/Documents/ Documents/
+    set music ~/Music/ Music/
+    set pictures ~/Pictures/ Pictures/
+    set videos ~/Videos/ Movies/
+
     if findmnt --types fuse.aft-mtp-mount | grep --fixed-strings --quiet /media/mtp
-        set options --recursive --inplace --size-only --delete --omit-dir-times --no-perms --verbose
-        echo -s $bold '> Syncing notes' $reset
-        rsync $options ~/Documents/Notes/ /media/mtp/SDカード/Documents/
+        set options --recursive --inplace --size-only --delete --omit-dir-times --no-perms --exclude='.*/' --verbose
+        echo -s $bold '> Syncing documents' $reset
+        rsync $options $documents[1] /media/mtp/SDカード/$documents[2]
         echo
         echo -s $bold '> Syncing musics' $reset
-        rsync $options ~/Music/ /media/mtp/SDカード/Music/
+        rsync $options $music[1] /media/mtp/SDカード/$music[2]
         echo
-        echo -s $bold '> Syncing photos' $reset
-        rsync $options ~/Pictures/Photos/ /media/mtp/SDカード/DCIM/Camera/Photos/
+        echo -s $bold '> Syncing pictures' $reset
+        rsync $options $pictures[1] /media/mtp/SDカード/$pictures[2]
         echo
         echo -s $bold '> Syncing videos' $reset
-        rsync $options ~/Videos/Camera/ /media/mtp/SDカード/DCIM/Camera/Videos/
+        rsync $options $videos[1] /media/mtp/SDカード/$videos[2]
         echo 'Finish!'
         return 0
     else if test (count $argv) -eq 3
-        set options --reverse --parallel=3 --ignore-time --delete --no-perms --verbose
+        set options --reverse --parallel=3 --ignore-time --delete --no-perms --exclude-glob='.*/' --verbose
         echo "Accessing $argv[3]@$argv[1]:$argv[2]"
         lftp -c "
             set cmd:fail-exit true;
             open -p $argv[2] -u $argv[3] $argv[1];
-            echo '> Syncing notes';
-            mirror $options ~/Documents/Notes/ /Documents;
-            echo '> Syncing musics';
-            mirror $options ~/Music/ /Music;
-            echo '> Syncing photos';
-            mirror $options ~/Pictures/Photos/ /DCIM/Camera/Photos;
+            echo '> Syncing documents';
+            mirror $options $documents[1] /$documents[2];
+            echo '> Syncing music';
+            mirror $options $music[1] /$music[2];
+            echo '> Syncing pictures';
+            mirror $options $pictures[1] /$pictures[2];
             echo '> Syncing videos';
-            mirror $options ~/Videos/Camera/ /DCIM/Camera/Videos;
+            mirror $options $videos[1] /$videos[2];
         "
         echo 'Finish!'
         return 0
     end
-
     echo 'FAIL: Phone not accessible.' >&2
     echo 'If using FTP, call with arguments: IP_ADDRESS PORT USER.' >&2
     return 1
