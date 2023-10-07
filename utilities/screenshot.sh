bf58e7ac5189644a6f3afe5dd406ceeef148fe06
Yutsuten <mateus.etto@proton.me>
2023-10-07 19:54:48 +0900

    Screenshot open on action

--- utilities/screenshot.sh
+++ utilities/screenshot.sh
@@ -9,20 +9,23 @@ set -u
 SCREENSHOT_DIR="${HOME}/Pictures/Screenshots"
 IMG_NAME="$(date '+%Y-%m-%d-%H-%M-%S_%N')"
 
-NOTIFY="notify-send --urgency low --app-name Screenshot"
+NOTIFY="notify-send --urgency low --app-name Screenshot --action=open"
+PLAY="pw-play /usr/share/sounds/freedesktop/stereo/screen-capture.oga"
 
 case $1 in
   --full)
     IMG_NAME="${IMG_NAME}-full.png"
     grim "${SCREENSHOT_DIR}/${IMG_NAME}"
-    ${NOTIFY} --icon "${SCREENSHOT_DIR}/${IMG_NAME}" 'Full screen capture' "${IMG_NAME}"
+    ${PLAY} &
+    ${NOTIFY} --icon "${SCREENSHOT_DIR}/${IMG_NAME}" 'Full screen capture' "${IMG_NAME}" | xargs test 0 -eq 2> /dev/null && vimiv "${SCREENSHOT_DIR}/${IMG_NAME}"
     ;;
   --active)
     IMG_NAME="${IMG_NAME}-active.png"
     FOCUSED=$(swaymsg -t get_tree | jq -r 'recurse(.nodes[]?, .floating_nodes[]?) | select(.focused)')
     GEOM=$(echo "$FOCUSED" | jq -r '.rect | "\(.x),\(.y) \(.width)x\(.height)"')
     grim -g "${GEOM}" "${SCREENSHOT_DIR}/${IMG_NAME}"
-    ${NOTIFY} --icon "${SCREENSHOT_DIR}/${IMG_NAME}" 'Active window capture' "${IMG_NAME}"
+    ${PLAY} &
+    ${NOTIFY} --icon "${SCREENSHOT_DIR}/${IMG_NAME}" 'Active window capture' "${IMG_NAME}" | xargs test 0 -eq 2> /dev/null && vimiv "${SCREENSHOT_DIR}/${IMG_NAME}"
     ;;
   --select)
     IMG_NAME="${IMG_NAME}-select.png"
@@ -31,12 +34,14 @@ case $1 in
       exit 1
     fi
     grim -g "${GEOM}" "${SCREENSHOT_DIR}/${IMG_NAME}"
-    ${NOTIFY} --icon "${SCREENSHOT_DIR}/${IMG_NAME}" 'Selected area capture' "${IMG_NAME}"
+    ${PLAY} &
+    ${NOTIFY} --icon "${SCREENSHOT_DIR}/${IMG_NAME}" 'Selected area capture' "${IMG_NAME}" | xargs test 0 -eq 2> /dev/null && vimiv "${SCREENSHOT_DIR}/${IMG_NAME}"
     ;;
   *)
     printf "Invalid option.\n" >&2
+    printf "Valid options: --full --active --select\n" >&2
     exit 1
     ;;
 esac
 
-pw-play /usr/share/sounds/freedesktop/stereo/screen-capture.oga
+exit 0
