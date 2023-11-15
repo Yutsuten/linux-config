df16e73c787faabb49b2a7c0b5096ff37b85d166
Yutsuten <mateus.etto@proton.me>
2023-11-15 22:22:17 +0900

    Update passgen command

--- fish/functions/passgen.fish
+++ fish/functions/passgen.fish
@@ -1,12 +1,35 @@
-function passgen --description 'Generate random passwords'
-    if test (count $argv) -eq 0 || test $argv[1] = '-h' || test $argv[1] = '--help'
-        echo 'Usage: passgen LENGTH [ALLOWED_CHARS|A-Za-z0-9!"#$]' >&2
+function passgen --argument-names ALLOWED_CHARS LENGTH --description 'Generate random passwords'
+    argparse --max-args 2 'h/help' -- $argv
+    or return
+
+    set DEFAULT_ALLOWED_CHARS '!-~'
+    set DEFAULT_LENGTH 25
+
+    function help --argument-names ALLOWED_CHARS LENGTH
+        echo 'Usage: passgen [-h|--help] [ALLOWED_CHARS] [LENGTH]' >&2
+        echo >&2
+        echo '  Defaults:' >&2
+        echo "  - ALLOWED_CHARS: '$ALLOWED_CHARS' (all characters, numbers and symbols)" >&2
+        echo "  - LENGTH: $LENGTH" >&2
+    end
+
+    if set --query --local _flag_help
+        help $DEFAULT_ALLOWED_CHARS $DEFAULT_LENGTH
         return 1
     end
-    if test (count $argv) -ne 2
-        set ALLOWED_CHARS 'A-Za-z0-9!"#$'
-    else
-        set ALLOWED_CHARS $argv[2]
+
+    if not test "$ALLOWED_CHARS"
+        set ALLOWED_CHARS $DEFAULT_ALLOWED_CHARS
     end
-    tr -dc $ALLOWED_CHARS < /dev/urandom | head -c$argv[1]
+
+    if test "$LENGTH" && not string match --quiet --regex '^[0-9]+$' "$LENGTH"
+        echo 'Error: LENGTH is not a number!' >&2
+        echo >&2
+        help $DEFAULT_ALLOWED_CHARS $DEFAULT_LENGTH
+        return 1
+    else if not test "$LENGTH"
+        set LENGTH $DEFAULT_LENGTH
+    end
+
+    tr -dc $ALLOWED_CHARS < /dev/urandom | head -c$LENGTH
 end
