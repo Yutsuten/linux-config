680cf15a977f1e0887285974db5f9e686cab4bff
Yutsuten <mateus.etto@proton.me>
2023-11-16 23:39:49 +0900

    Update system utility

--- utilities/system.sh
+++ utilities/system.sh
@@ -11,28 +11,37 @@ img:/usr/share/icons/Arc/panel/22/system-devices-panel.svg:text:Poweroff' \
   | wofi --dmenu --prompt '' --cache-file /dev/null \
   | cut -d ':' -f 4)
 
+want_to() {
+  if findmnt --output TARGET --noheadings --raw | grep --quiet '^/media'; then
+    notify-send --app-name system --icon dialog-warning --urgency critical \
+      "CANNOT $1" \
+      "There is media mounted at \n$(findmnt --output TARGET --noheadings --raw | grep '^/media')"
+    exit 1
+  fi
+}
+
 case ${option} in
   'Lock')
-    swaylock
+    swaylock --image ~/Pictures/Wallpapers/"$(head -n 1 ~/.cache/wallpaper)"
     ;;
   'Logout')
     swaymsg exit
     ;;
   'Sleep')
+    want_to SLEEP
     systemctl suspend
     ;;
   'Hibernate')
-    if findmnt --output TARGET --noheadings --raw | grep --quiet '^/media'; then
-      echo 'Cannot hibernate with media mounted.' >&2
-      exit 1
-    fi
+    want_to HIBERNATE
     wpctl set-volume @DEFAULT_AUDIO_SINK@ 50%
     systemctl hibernate
     ;;
   'Reboot')
+    wpctl set-volume @DEFAULT_AUDIO_SINK@ 50%
     systemctl reboot
     ;;
   'Poweroff')
+    wpctl set-volume @DEFAULT_AUDIO_SINK@ 50%
     systemctl poweroff
     ;;
   *)
