71319ee217749a322055631cacb9c57895450d9b
Yutsuten <mateus.etto@proton.me>
2023-11-20 21:57:55 +0900

    Export environment variables with fish

--- fish/sway.fish
+++ fish/sway.fish
@@ -1,12 +1,9 @@
-set TTY1 (tty)
-if [ "$TTY1" = "/dev/tty1" ]
-    # GTK theme
+if test (tty) = "/dev/tty1"
     gsettings set org.gnome.desktop.interface gtk-theme 'Arc'
     gsettings set org.gnome.desktop.interface icon-theme 'Arc'
     gsettings set org.gnome.desktop.interface font-name 'Noto Sans 12'
     gsettings set org.gnome.desktop.interface cursor-theme 'Vimix-cursors'
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-
-    # Start sway
+    source ~/.local/environment
     exec sway
 end
