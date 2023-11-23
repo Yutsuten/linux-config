5799bac968c73ead12d9c976e7a1b3e669c9c46d
Yutsuten <mateus.etto@proton.me>
2023-11-23 19:08:58 +0900

    Update record script
    Mix mic and speakers at the end

--- tools/fish/functions/record.fish
+++ tools/fish/functions/record.fish
@@ -2,7 +2,7 @@ function record --description 'Record screen using wf-recorder'
     argparse 'h/help' 'm/mic' 'r/rec' 's/speakers' -- $argv
     set exitcode $status
 
-    if test $exitcode -ne 0 || test $_flag_h
+    if test $exitcode -ne 0 || set --query --local _flag_help
         echo 'Usage: record [options] [name]' >&2
         echo '  -h, --help      Show list of command-line options'
         echo '  -m, --mic       Record mic'
@@ -18,18 +18,18 @@ function record --description 'Record screen using wf-recorder'
     echo "[INFO] Will record in directory $folder_name/"
     mkdir $folder_name
 
-    if test $_flag_mic
+    if set --query --local _flag_mic
         echo '[AUDIO] Start recording mic'
         command ffmpeg -loglevel warning -nostdin -f pulse -i (pactl list short sources | sed -nE 's/^.*(alsa_input.+analog-stereo[^\s\t]*).*$/\1/p') $folder_name/mic.flac &
         set mic_pid $last_pid
     end
-    if test $_flag_rec
+    if set --query --local _flag_rec
         echo '[AUDIO] Start recording "recording" source'
         pactl set-sink-volume recording 100%
         command ffmpeg -loglevel warning -nostdin -f pulse -i recording.monitor $folder_name/rec.flac &
         set rec_pid $last_pid
     end
-    if test $_flag_speakers
+    if set --query --local _flag_speakers
         echo '[AUDIO] Start recording speakers'
         command ffmpeg -loglevel warning -nostdin -f pulse -i (pactl list short sources | sed -nE 's/^.*(alsa_output.+analog-stereo\.monitor[^\s\t]*).*$/\1/p') $folder_name/speakers.flac &
         set speakers_pid $last_pid
@@ -39,19 +39,27 @@ function record --description 'Record screen using wf-recorder'
     nice -n -5 wf-recorder -c libx264rgb -p crf=0 -f $folder_name/video.mp4
     echo '[VIDEO] Stop recording wayland screen'
 
-    if test $_flag_mic
+    if set --query --local _flag_mic
         echo '[AUDIO] Stop recording mic'
         kill --verbose $mic_pid
     end
-    if test $_flag_rec
+    if set --query --local _flag_rec
         echo '[AUDIO] Stop recording "recording" source'
         kill --verbose $rec_pid
     end
-    if test $_flag_speakers
+    if set --query --local _flag_speakers
         echo '[AUDIO] Stop recording speakers'
         kill --verbose $speakers_pid
     end
 
-    echo 'Finish!'
     sleep 0.2
+    if set --query --local _flag_mic && set --query --local _flag_speakers
+        echo '[AUDIO] Mixing mic and speakers'
+        nice -n 5 ffmpeg -loglevel warning -i $folder_name/mic.flac -i $folder_name/speakers.flac \
+          -filter_complex \
+           '[0:a][0:a]amerge=inputs=2[0a];
+            [0a][1:a]amerge=inputs=2[outa]' -map '[outa]' \
+          $folder_name/mix.flac
+    end
+    echo 'Finish!'
 end
