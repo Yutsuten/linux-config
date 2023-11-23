7fdf8eb19a615aa27e5da8ce6018766d6ae6efea
Yutsuten <mateus.etto@proton.me>
2023-11-23 10:05:57 +0900

    Fix pipewire 90-init

--- desktop/pipewire/90-init.sh
+++ desktop/pipewire/90-init.sh
@@ -5,6 +5,6 @@ set -u
 
 cat > ~/.config/pipewire/pipewire-pulse.conf.d/90-init.conf << PIPEWIRE_INIT
 context.exec = [
-    { path = "/usr/bin/bash" args = "$(readlink -e window_manager/pipewire/init.sh)" }
+    { path = "/usr/bin/bash" args = "$(readlink -e desktop/pipewire/init.sh)" }
 ]
 PIPEWIRE_INIT
