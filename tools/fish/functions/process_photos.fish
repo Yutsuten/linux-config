8faa810a60d8ebd4275a19cfe468d3f7d84f0e86
Mateus Etto <mateus.etto@proton.me>
2023-11-30 00:07:31 +0900

    Update process_photos convert to webp and resize

--- tools/fish/functions/process_photos.fish
+++ tools/fish/functions/process_photos.fish
@@ -1,8 +1,8 @@
 function process_photos --description 'Process photos from phone'
     for photo in *.jpg
-        set newname (exiv2 $photo | sed -nE 's/Image timestamp : ([0-9]{4}):([0-9]{2}):([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})/\1-\2-\3-\4-\5-\6.jpg/p')
+        set newname (exiv2 $photo | sed -nE 's/Image timestamp : ([0-9]{4}):([0-9]{2}):([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})/\1-\2-\3-\4-\5-\6.webp/p')
         if test "$photo" != "$newname"
-            mv "$photo" "$newname"
+            convert "$photo" -resize '2000x2000>' -define webp:method=6 "$newname"
         end
     end
 end
