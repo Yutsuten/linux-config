3f2a49138a101688981ada63fb7880715b676308
Mateus Etto <mateus.etto@proton.me>
2024-02-26 13:56:23 +0900

    Refactor bkptool

--- tools/fish/functions/bkptool.fish
+++ tools/fish/functions/bkptool.fish
@@ -32,55 +32,32 @@ function bkptool --description 'Backup and restore user files'
             rsync --archive --update --delete --verbose "$bkp_dir/$dir/" "$HOME/$dir/"
         end
 
-        echo $bold"Restore Config.tar.zst.gpg"$reset
-        gpg --decrypt "$bkp_dir/Config.tar.zst.gpg" \
-            | tar --extract --zstd --directory ~
-
-        echo $bold"Restore Data.tar.zst.gpg"$reset
-        gpg --decrypt "$bkp_dir/Data.tar.zst.gpg" \
+        echo $bold"Restore Linux.tar.zst.gpg"$reset
+        gpg --decrypt "$bkp_dir/Linux.tar.zst.gpg" \
             | tar --extract --zstd --directory ~
     else
-        # Sync - Check for empty directories
-        set empty 0
+        # Sync backup
         for dir in $sync_dirs
-            test -z (find "$HOME/$dir" -maxdepth 0 -empty) || set empty 1
-        end
-        if test $empty -eq 1
-            echo 'Will not backup empty directory. Abort.' >&2
-            return 1
-        end
-
-        # Sync - Perform backup
-        for dir in $sync_dirs
-            echo $bold"Backup $dir"$reset
-            rsync --archive --update --delete --verbose "$HOME/$dir/" "$bkp_dir/$dir/"
+            if test -z (find "$HOME/$dir" -maxdepth 0 -empty)
+                echo $bold"Backup $dir"$reset
+                rsync --archive --update --delete --verbose "$HOME/$dir/" "$bkp_dir/$dir/"
+            else
+                echo $bold"Skip empty $dir"$reset
+            end
         end
 
-        # Compressed encrypted backup - Generate files
-        set bkp_config .config/backup_config
+        # Compressed encrypted backup
+        set bkp_linux .config/backup.list
         while read --line target
-            set --append bkp_config $target
-        end < ~/.config/backup_config
+            set --append bkp_linux $target
+        end < ~/.config/backup.list
 
-        set bkp_data .config/backup_data
-        while read --line target
-            set --append bkp_data $target
-        end < ~/.config/backup_data
+        echo $bold'Generate Linux.tar.zst.gpg'$reset
+        tar --create --zstd --directory ~ $bkp_linux \
+          | gpg --encrypt --default-recipient-self > Linux.tar.zst.gpg
 
-        echo $bold'Generate encrypted backup of config'$reset
-        tar --create --zstd --directory ~ $bkp_config \
-          | gpg --encrypt --default-recipient-self > Config.tar.zst.gpg
-
-        echo $bold'Generate encrypted backup of data'$reset
-        tar --create --zstd --directory ~ $bkp_data \
-          | gpg --encrypt --default-recipient-self > Data.tar.zst.gpg
-
-        # Compressed encrypted backup - Perform backup
-        echo $bold'Backup Config.tar.zst.gpg'$reset
-        cp --archive Config.tar.zst.gpg "$bkp_dir/Config.tar.zst.gpg"
-
-        echo $bold'Backup Data.tar.zst.gpg'$reset
-        cp --archive Data.tar.zst.gpg "$bkp_dir/Data.tar.zst.gpg"
+        echo $bold'Backup Linux.tar.zst.gpg'$reset
+        cp --archive Linux.tar.zst.gpg "$bkp_dir/Linux.tar.zst.gpg"
     end
     echo $bold'Finish!'$reset
     return 0
