b1c49f22dda505c99d9023bddced42d421732680
Mateus Etto <mateus.etto@proton.me>
2024-05-02 12:32:21 +0900

    Adjusts to fish share function

--- tools/fish/functions/share.fish
+++ tools/fish/functions/share.fish
@@ -1,9 +1,9 @@
 function share --description 'Share screen or phone camera using v4l2loopback'
-    argparse --max-args 1 'h/help' 'f/flip' -- $argv
+    argparse --max-args 3 'h/help' 'f/flip' -- $argv
     set exitcode $status
 
     function help
-        echo 'Usage: share TARGET' >&2
+        echo 'Usage: share TARGET [IP] [PORT]' >&2
         echo >&2
         echo '  Synopsis:' >&2
         echo '    Share screen or phone camera using v4l2loopback.' >&2
@@ -13,7 +13,7 @@ function share --description 'Share screen or phone camera using v4l2loopback'
         echo '    -f, --flip      Apply horizontal flip' >&2
         echo >&2
         echo '  Positional arguments:' >&2
-        echo "    TARGET          Either 'camera' or 'screen'" >&2
+        echo "    TARGET          Either 'camera' or 'screen'. If using 'camera', specify IP and PORT too." >&2
     end
 
     if test $exitcode -ne 0 || set --query --local _flag_help
@@ -25,20 +25,20 @@ function share --description 'Share screen or phone camera using v4l2loopback'
     switch $argv[1]
         case camera
             sudo modprobe -r v4l2loopback
-            sudo modprobe v4l2loopback
+            sudo modprobe v4l2loopback card_label='DroidCam' exclusive_caps=1
             sleep 0.1
             if set --query --local _flag_flip
                 set flip -hflip
             end
-            droidcam-cli $flip 192.168.0.110 4747
+            droidcam-cli $flip -size=1920x1080 $argv[2] $argv[3]
         case screen
             sudo modprobe -r v4l2loopback
-            sudo modprobe v4l2loopback
+            sudo modprobe v4l2loopback card_label='Wayland Screen' exclusive_caps=1
             sleep 0.1
             if set --query --local _flag_flip
                 set flip -F hflip
             end
-            wf-recorder --muxer=v4l2 --codec=rawvideo --file=/dev/video0 -x yuv420p $flip
+            wf-recorder --muxer=v4l2 --codec=rawvideo --file=/dev/video0 --pixel-format=yuv420p $flip
         case '*'
             help
             return 1
