22956308b90d6a9b8bd33f4fd4c886ddc9c8a889
Mateus Etto <mateus.etto@proton.me>
2024-05-24 19:11:00 +0900

    Fix nnn not getting arguments

--- tools/fish/functions/nnn.fish
+++ tools/fish/functions/nnn.fish
@@ -1,3 +1,3 @@
 function nnn --wraps=nnn --description 'The unorthodox terminal file manager.'
-    test -f ~/.local/bin/nnn && source ~/.local/bin/nnn || command nnn
+    test -f ~/.local/bin/nnn && ~/.local/bin/nnn $argv || command nnn $argv
 end
