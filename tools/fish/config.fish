19bdb74c52a9428dc1a07865c7b5489010e8775d
Mateus Etto <mateus.etto@proton.me>
2024-06-17 21:14:28 +0900

    Remove ls abbr

--- tools/fish/config.fish
+++ tools/fish/config.fish
@@ -41,7 +41,6 @@ if status is-interactive
     abbr --add l1        -- 'ls -N1 --sort=v --group-directories-first'
     abbr --add ll        -- 'ls -Nlh --sort=v --group-directories-first'
     abbr --add lo        -- 'ls -Noh --sort=v --group-directories-first'
-    abbr --add ls        -- 'ls -N --sort=v --group-directories-first'
     abbr --add ssh       -- 'env TERM=xterm-256color ssh'
     abbr --add vimiv     -- 'vimiv --log-level error'
 
