90d04972c89dce8a5276f2b74aa3ce279480ac3e
Mateus Etto <mateus.etto@proton.me>
2024-09-29 16:15:24 +0900

    mpv show osc on pause toggle too

--- tools/mpv/scripts/osc.lua
+++ tools/mpv/scripts/osc.lua
@@ -1,3 +1,6 @@
-mp.register_event('seek', function ()
-    mp.commandv('script-message', 'osc-show')
-end)
+function show_osc()
+    mp.commandv("script-message", "osc-show")
+end
+
+mp.register_event("seek", show_osc)
+mp.observe_property("pause", "bool", show_osc);
