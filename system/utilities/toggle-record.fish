b89b591161ce9f9caae4c722c963530f437f97dc
Mateus Eto <mateus.etto@gmail.com>
2024-11-18 00:12:28 +0900

    toggle-record toggle re-encoding automatically

--- system/utilities/toggle-record.fish
+++ system/utilities/toggle-record.fish
@@ -2,14 +2,18 @@
 
 if pgrep --full 'fish /usr/local/bin/record'
     # Stop recording
-    pkill --signal SIGINT --full 'fish /usr/local/bin/record' &
+    pkill --signal INT --full 'fish /usr/local/bin/record'
     notify-send --urgency low --app-name Record --icon process-completed-symbolic 'Finished recording screen' &
     pw-play /usr/share/sounds/freedesktop/stereo/service-logout.oga &
+    pkill --signal CONT ffmpeg # Continue video reencoding if we stopped any
 else
     # Start recording
     record-settings
+    pkill --signal STOP ffmpeg # Stop any video reencoding if any
     mkdir --parents ~/.local/logs
     record &>~/.local/logs/record.log &
     notify-send --urgency low --app-name Record --icon software-update-urgent-symbolic 'Recording screen' &
     pw-play /usr/share/sounds/freedesktop/stereo/service-login.oga &
 end
+
+return 0
