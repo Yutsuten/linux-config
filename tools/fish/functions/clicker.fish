46c701aa96ada94d4c127b436044c36d6dfb76a9
Mateus Eto <mateus.etto@gmail.com>
2025-01-08 00:27:35 +0900

    Reduce default sleep for clicker

--- tools/fish/functions/clicker.fish
+++ tools/fish/functions/clicker.fish
@@ -16,7 +16,7 @@ function clicker --description 'Auto clicker for Sway'
     sleep $argv[1] || return
     while true
         swaymsg seat seat0 cursor press button1
-        sleep 0.05s
+        sleep 0.02s
         swaymsg seat seat0 cursor release button1
         sleep $argv[1]
     end
