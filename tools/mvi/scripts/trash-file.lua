dd4cc75329bfe4a9c265c87e569308e162c57c29
Mateus Eto <mateus.etto@gmail.com>
2025-04-05 11:45:52 +0900

    mvi trash print local path, nnn/mount unmount run lsblk

--- tools/mvi/scripts/trash-file.lua
+++ tools/mvi/scripts/trash-file.lua
@@ -1,6 +1,8 @@
 local msg = require 'mp.msg'
 local utils = require "mp.utils"
 
+local cwd = mp.get_property_native("working-directory")
+
 function trash_file()
     local current = mp.get_property_native("path")
     msg.warn("Delete " .. current)
@@ -15,7 +17,8 @@ function trash_file()
         return
     end
     mp.commandv("playlist-remove", "current")
-    mp.commandv("show-text", string.format("Deleted '%s'", current), 2000)
+    local current_short = string.gsub(current, string.format("^%s/", cwd), "")
+    mp.commandv("show-text", string.format("Deleted '%s'", current_short), 2000)
 end
 
 mp.add_key_binding("x", "trash-file", trash_file)
