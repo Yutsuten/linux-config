0cf343bde3a5b522253bc407379365ee57208232
Mateus Eto <mateus.etto@gmail.com>
2025-05-05 21:27:17 +0900

    Improve mpv delete current logic

--- tools/mpv/scripts/playlist.lua
+++ tools/mpv/scripts/playlist.lua
@@ -2,31 +2,42 @@ local msg = require 'mp.msg'
 local utils = require 'mp.utils'
 
 local cwd = mp.get_property_native("working-directory")
+local msg_time = 2000
 
 function remove_current()
     local playlist_path = mp.get_property_native("playlist-path")
     if not playlist_path then
         return
     end
+
     local current = mp.get_property_native("path")
-    msg.warn(string.format("%s: Remove %s", playlist_path, current))
+    local current_short = string.gsub(current, string.format("^%s/", cwd), "")
+    local playlist_before_length = 0
     local playlist_after = {}
+
     for music_path in io.lines(playlist_path) do
-        if music_path ~= current then
+        playlist_before_length = playlist_before_length + 1
+        if music_path ~= current and music_path ~= current_short then
             table.insert(playlist_after, music_path)
         end
     end
-    local playlist_file = io.open(playlist_path, "w")
-    if playlist_file then
-        playlist_file:write(table.concat(playlist_after, "\n") .. "\n")
-        playlist_file:close()
+
+    if #playlist_after < playlist_before_length then
+        msg.warn(string.format("%s: Remove %s", playlist_path, current))
+        local playlist_file = io.open(playlist_path, "w")
+        if playlist_file then
+            playlist_file:write(table.concat(playlist_after, "\n") .. "\n")
+            playlist_file:close()
+            mp.commandv("show-text", string.format("Removed from playlist: %s", current_short), msg_time)
+            mp.commandv("playlist-remove", "current")
+        else
+            msg.error("Failed to open playlist file")
+            mp.commandv("show-text", "Failed to remove from playlist: Cannot open playlist file", msg_time)
+        end
     else
-        msg.error("Failed to remove music")
-        return
+        msg.error(string.format("%s: Not found for removal current=%s cwd=%s", playlist_path, current, cwd))
+        mp.commandv("show-text", string.format("Failed to remove from playlist: %s", current_short), msg_time)
     end
-    mp.commandv("playlist-remove", "current")
-    local current_short = string.gsub(current, string.format("^%s/", cwd), "")
-    mp.commandv("show-text", string.format("Removed '%s' from playlist", current_short), 2000)
 end
 
 mp.add_key_binding("D", "remove-from-playlist", remove_current)
