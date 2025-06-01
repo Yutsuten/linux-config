51c12924b0563e89d79602043b056d9182f4d6db
Mateus Eto <mateus.etto@gmail.com>
2025-06-02 00:59:36 +0900

    Improvements to zellij-exec and neomuttrc

--- desktop/pipewire/configure.fish
+++ desktop/pipewire/configure.fish
@@ -2,5 +2,5 @@ mkdir --parents ~/.config/pipewire/pipewire-pulse.conf.d
 
 set init_abs_path (path resolve desktop/pipewire/init.sh)
 
-ln -f desktop/pipewire/10-remap-sink.conf ~/.config/pipewire/pipewire-pulse.conf.d/10-remap-sink.conf
+ln -nf desktop/pipewire/10-remap-sink.conf ~/.config/pipewire/pipewire-pulse.conf.d/10-remap-sink.conf
 sed -e "s#{{ init-abs-path }}#$init_abs_path#" desktop/pipewire/90-init.conf >~/.config/pipewire/pipewire-pulse.conf.d/90-init.conf
